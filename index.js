2b4e44c26efaf2c1580a88a91d486085988bb96a
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-06-17 10:55:46 +0300

    commit tree 149c864780e1e380d6c8b59434769b9f06b4da0f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(src, sought) {
+  return src.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+  );
+}
+
+function fuzzyMatch(src, testString) {
+  return src.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(src, soughtName) {
+  return src.filter((record) => record.name === soughtName);
+}
